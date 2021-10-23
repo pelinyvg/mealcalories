b3a13fa4fc792bd7e34d92b81dba5588cefd56dd
pelinyvg <pelin.yuzbasi@gmail.com>
2021-10-23 14:52:54 +0200

    second commit

---
+++ src/main/java/com/outlin/mealcalories/MealcaloriesApplication.java
@@ -0,0 +1,13 @@
+package com.outlin.mealcalories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealcaloriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealcaloriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/outlin/mealcalories/MealcaloriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outlin.mealcalories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealcaloriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
